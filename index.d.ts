f6e8ab8a0d2d1ad41fa697c63396ac45462abe1f
Ariel Flesler <aflesler@gmail.com>
2020-09-21 11:59:57 -0300

    Fixed index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module 'infer-gender' {
 
 	export function initialize(): void
 
-	export function extractName(fullName: string, onlyFirst = false): string
+	export function extractName(fullName: string, onlyFirst?: boolean): string
 
-	export function infer(fullName: string, onlyFirst = false): 'male' | 'female' | 'unisex' | 'unknown'
+	export function infer(fullName: string, onlyFirst?: boolean): 'male' | 'female' | 'unisex' | 'unknown'
 }
