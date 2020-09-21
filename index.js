0c80c53de6dc32f52bda6da83d3e101cc1988e38
Ariel Flesler <aflesler@gmail.com>
2020-09-21 10:59:23 -0300

    Names can be loaded on-demand or manually force-loaded

--- index.js
+++ index.js
@@ -11,24 +11,34 @@ function load(gender) {
 	return fs.readFileSync(path, 'utf8').trim().split(/\r?\n/)
 }
 
-const names = {}
-for (const name of load(MALE)) {
-	names[name] = MALE
+let names
+
+exports.initialize = () => {
+	names = {}
+
+	for (const name of load(MALE)) {
+		names[name] = MALE
+	}
+
+	for (const name of load(FEMALE)) {
+		names[name] = name in names ? UNISEX : FEMALE
+	}
 }
 
-for (const name of load(FEMALE)) {
-	names[name] = name in names ? UNISEX : FEMALE
-}
-
-exports.extractName = (fullName, onlyFirst = false) => (
-	removeDiacritics(fullName)
+exports.extractName = (fullName, onlyFirst = false) => {
+	if (!names) {
+		// If not manually initialized, load in-demand
+		exports.initialize()
+	}
+	return removeDiacritics(fullName)
 		.toLowerCase()
 		.replace(/[^a-z-абвгдезиклмнопрстэюяабвгдеийклмнопрстуфхчьяё ]+/g, '')
 		.split(' ')
 		// Return the first word that matches a name
 		.find(name => name in names || onlyFirst)
-)
+}
 
-exports.infer = (fullName, onlyFirst = false) => (
-	names[exports.extractName(fullName, onlyFirst)] || UNKNOWN
-)
+exports.infer = (fullName, onlyFirst = false) => {
+	const name = exports.extractName(fullName, onlyFirst)
+	return names[name] || UNKNOWN
+}
